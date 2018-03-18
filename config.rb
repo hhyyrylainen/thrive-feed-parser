613f97bb20da47b68cee39a7372fa93b42fc91e3
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-03-18 17:17:18 +0200

    Added github feed

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+GithubFeedToken = ENV['GITHUB_FEED_TOKEN'] || ""
+
 # Modify the feed parameters here
 Feeds = [
   {
@@ -5,19 +7,32 @@ Feeds = [
     url: "https://forum.revolutionarygamesstudio.com/posts.rss",
     maxItems: 10,
   },
+  if !GithubFeedToken.empty?
+    {
+      name: "github-revolutionarygames",
+      url: "https://github.com/organizations/Revolutionary-Games/" +
+      "revolutionary-bot.private.atom?token=#{GithubFeedToken}",
+      maxItems: 15,
+    }
+  end,
   {
     name: "community-announcements",
     url: "https://community.revolutionarygamesstudio.com/c/announcements.rss",
     maxItems: 1,
     maxLength: 300,
   },
-]
+  if !GithubFeedToken.empty?
+    # Combined feed
+    {
+      name: "devforum-and-github",
+      combine: ["devforum-posts", "github-revolutionarygames"],
+      maxItems: 30,
+    }
+  end,
+].select{|i| !i.nil?}
 
 # Time to wait after parsing feeds is complete before doing again
 UpdateEveryNSeconds = 900
 
-TargetFolder = if ENV['TARGET_FOLDER']
-                 ENV['TARGET_FOLDER']
-               else
-                 "./html"
-               end
+TargetFolder = ENV['TARGET_FOLDER'] || "./html"
+
--- thrive-feeds.rb
+++ thrive-feeds.rb
@@ -22,7 +22,7 @@ FileUtils.mkdir_p TargetFolder
 puts "Starting RSS feed to html bot for thrive places"
 puts "Writing to folder: #{TargetFolder}"
 
-encoder = HTMLEntities.new
+$encoder = HTMLEntities.new
 
 $runFeeds = true
 
@@ -32,6 +32,94 @@ Signal.trap("INT") {
   $feedThread.wakeup
 }
 
+$latestItems = {}
+
+def outputItem(feed, file, item)
+  # Try to get rid of script tags if they are there
+  text = if item.summary? then
+           item.summary else
+           item.content end.gsub /<script>/i, "&lt;script&gt;"
+
+  file.puts %{<div class="thrive-feed-item thrive-feed-name-#{feed[:name]}">} +
+            %{<span class="thrive-feed-title"><span class="thrive-feed-title-main">} +
+            %{<a class="thrive-feed-title-link" href="#{item.url}">} +
+            $encoder.encode(item.title) + "</a>" +
+            %{</span><span class="thrive-feed-by"> by } +
+            %{<span class="thrive-feed-author">} +
+            $encoder.encode(item.author.to_s.split(' ')[0]) + "</span></span>" + 
+            %{<span class="thrive-feed-at"> at <span class="thrive-feed-time">} +
+            $encoder.encode(item.published) +
+            "</span></span>" +
+            %{</span><br><span class="thrive-feed-content">} +
+            truncate_html(text, length: if feed.include?(:maxLength) then
+                           feed[:maxLength] else 150 end,
+                          omission: '...(continued)') +
+            %{<br><a class="thrive-feed-item-url" href="#{item.url}">} +
+            #%{#{$encoder.encode(item.url)}</a></span></div>}
+            %{Read it here</a></span></div>}
+end
+
+def outputFeedItems(feed, items)
+  File.open(File.join(TargetFolder, feed[:name] + ".html"), 'w'){|file|
+    
+    itemNum = 0
+    
+    items.each{|item|
+
+      outputItem feed, file, item
+      
+      itemNum += 1
+
+      if itemNum >= feed[:maxItems]
+        break
+      end
+    }      
+  }
+end
+
+def handleURLFeed(feed)
+
+  puts "Retrieving feed: " + feed[:name]
+
+  feedParser = FeedParser::Parser.parse(open(feed[:url]).read)
+  items = feedParser.items
+
+  if items.nil?
+    puts "Failed to retrieve feed: " + feed[:name]
+    return
+  end
+
+  # Store the items for combines
+  $latestItems[feed[:name]] = items
+
+  outputFeedItems feed, items
+  
+end
+
+def handleCombinedFeed(feed)
+
+  puts "Handling combined feed: " + feed[:name] + " (#{feed[:combine]})"
+
+  # Find all the items
+  items = []
+
+  feed[:combine].each{|subfeed|
+
+    if !$latestItems.include?(subfeed)
+      puts "Combined feed '#{feed[:name]}' sub feed has no items: " + subfeed
+      return
+    end
+    items += items + $latestItems[subfeed]
+  }
+
+  # Sort them by time
+  items = items.sort_by{|i| i.published}.reverse
+
+  # And output
+  outputFeedItems feed, items
+  
+end
+
 $feedThread = Thread.new {
 
   while $runFeeds
@@ -39,48 +127,24 @@ $feedThread = Thread.new {
 
     Feeds.each{|feed|
 
-      puts "Retrieving feed: " + feed[:name]
-
-      feedParser = FeedParser::Parser.parse(open(feed[:url]).read)
-      items = feedParser.items
-
-      File.open(File.join(TargetFolder, feed[:name] + ".html"), 'w'){|file|
-
-        itemNum = 0
+      if feed.include?(:url)
         
-        items.each{|item|
-
-          # Try to get rid of script tags if they are there
-          text = item.summary.gsub /<script>/i, "&lt;script&gt;"
-
-          file.puts %{<div class="thrive-feed-item thrive-feed-name-#{feed[:name]}">} +
-                    %{<span class="thrive-feed-title"><span class="thrive-feed-title-main">} +
-                    %{<a class="thrive-feed-title-link" href="#{item.url}">} +
-                    encoder.encode(item.title) + "</a>" +
-                    %{</span><span class="thrive-feed-by"> by } +
-                    %{<span class="thrive-feed-author">} +
-                    encoder.encode(item.author.to_s.split(' ')[0]) + "</span></span>" + 
-                    %{<span class="thrive-feed-at"> at <span class="thrive-feed-time">} +
-                    encoder.encode(item.published) +
-                    "</span></span>" +
-                    %{</span><br><span class="thrive-feed-content">} +
-                    truncate_html(text, length: if feed.include?(:maxLength) then
-                                   feed[:maxLength] else 150 end,
-                                  omission: '...(continued)') +
-                    %{<br><a class="thrive-feed-item-url" href="#{item.url}">} +
-                    #%{#{encoder.encode(item.url)}</a></span></div>}
-                    %{Read it here</a></span></div>}
-          
-          itemNum += 1
-
-          if itemNum >= feed[:maxItems]
-            break
-          end
-        }      
-      }
+        handleURLFeed feed
+        
+      elsif feed.include?(:combine)
+        
+        handleCombinedFeed feed
+        
+      else
+        puts "Unkown feed type: " + feed
+        exit 1
+      end
     }
 
     puts "Done handling feeds. Waiting for next run"
+    
+    # Clear the latest items to save some memory
+    $latestItems = {}
 
     # And allow quitting before waiting
     if !$runFeeds
